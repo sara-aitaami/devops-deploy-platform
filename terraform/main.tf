94ee934947971707aef45ac990ef8e18c0d49f84
sara-aitaami <sara.aitaami@gmail.com>
2026-08-31 19:34:07 +0200

    feat: manage API service with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,26 @@ resource "kubernetes_config_map" "terraform_info" {
     environment = var.environment
   }
 }
+
+resource "kubernetes_service" "devops_api" {
+  wait_for_load_balancer = false
+
+  metadata {
+    name      = "devops-api"
+    namespace = kubernetes_namespace.devops.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "devops-api"
+    }
+
+    port {
+      port        = 8000
+      target_port = 8000
+      node_port   = 30080
+    }
+
+    type = "NodePort"
+  }
+}
